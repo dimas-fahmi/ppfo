c0c44d09e27b9b6d2d8dd3628448194ce007824e
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-09-05 19:43:04 +0700

    Add font imports and configurations for Inter and Geist Mono

---
+++ src/ui/fonts/index.ts
@@ -0,0 +1,13 @@
+import { Geist_Mono, Inter } from "next/font/google";
+
+export const interFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const geistMonoFont = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
