3dc9927fa385d841736f127c632060ab5ce7c2d9
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-09-08 22:29:41 +0700

    refactor to use dedicated policy

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,4 +1,8 @@
-import { pgPolicy, pgSchema } from "drizzle-orm/pg-core";
+// Imports module
+import { pgSchema } from "drizzle-orm/pg-core";
+
+// Import Models
+import { profiles } from "./profiles";
 
 // Enum values
 const moderationLevel = [
@@ -9,40 +13,10 @@ const moderationLevel = [
   "scheduled",
 ] as const;
 
-// Import Models
-import { profiles } from "./profiles";
-import { anonRole, authenticatedRole, serviceRole } from "drizzle-orm/supabase";
-import { sql } from "drizzle-orm";
-
 // Schemas
 const userManagement = pgSchema("user_management");
 const organizationSchema = pgSchema("organization");
 const postSchema = pgSchema("post");
-
-// Preset Policies
-const policy_AuthenticatedCanInsert = pgPolicy("Authenticated can insert", {
-  as: "permissive",
-  to: authenticatedRole,
-  for: "insert",
-  using: sql``,
-  withCheck: sql``,
-});
-
-const policy_OnlyServiceCanUpdate = pgPolicy("Only Service can update", {
-  as: "restrictive",
-  to: serviceRole,
-  for: "all",
-  using: sql``,
-  withCheck: sql``,
-});
-
-const policy_AnyoneCanRead = pgPolicy("Anyone can read", {
-  as: "permissive",
-  to: anonRole,
-  for: "select",
-  using: sql``,
-  withCheck: sql``,
-});
 
 // Export
 export {
@@ -56,9 +30,4 @@ export {
 
   // Models
   profiles,
-
-  // Policies
-  policy_AuthenticatedCanInsert,
-  policy_OnlyServiceCanUpdate,
-  policy_AnyoneCanRead,
 };
