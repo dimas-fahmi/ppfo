d07860f5b3ea0035c948854ae3e1a874d44ddbc5
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-09-11 14:36:07 +0700

    fix responsive issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Sidebar from "@/src/ui/components/NavBar/Sidebar";
 import { useSidebarStore } from "@/src/lib/stores/sidebar";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useMediaQuery } from "react-responsive";
 
 export default function RootLayout({
   children,
@@ -18,13 +19,18 @@ export default function RootLayout({
   // Sidebar open State
   const sidebarOpenState = useSidebarStore((state) => state.open);
 
+  // Query Media
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
   return (
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="PPFO" />
       </head>
       <body
-        className={`${sidebarOpenState ? "ms-[320px]" : ""} ${
+        className={`${isDesktop && sidebarOpenState ? "ms-[320px]" : ""} ${
           interFont.variable
         } ${geistMonoFont.variable} ${
           oswaldFont.variable
