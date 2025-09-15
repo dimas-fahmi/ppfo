7a6cd945f8123a5f04cd10cff757f9f9039cec5f
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-09-15 09:42:24 +0700

    initialize pages

--- src/lib/fetchers/fetchUserProfile.ts
+++ src/lib/fetchers/fetchUserProfile.ts
@@ -2,6 +2,9 @@ import { UsersProfile } from "@/app/api/users/profiles/get";
 
 export const fetchUserProfile = async (id: string): Promise<UsersProfile> => {
   try {
+    if (!id) {
+      throw new Error("ID is not yet exist, aborted.");
+    }
     const response = await fetch(`/api/users/profiles/?id=${id}`);
 
     const result = await response.json();
