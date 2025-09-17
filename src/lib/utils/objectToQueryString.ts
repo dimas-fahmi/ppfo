5cc4b2029950143fc105fc937fcb06ab12789324
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-09-17 13:03:02 +0700

    modifiy to allow empty object without error

--- src/lib/utils/objectToQueryString.ts
+++ src/lib/utils/objectToQueryString.ts
@@ -3,19 +3,20 @@
  * The resulting string will have the form: `key1=value1&key2=value2&...`.
  * Both keys and values are URL-encoded to ensure proper formatting.
  *
- * @param {Record<string, any>} obj - The object to be converted into a query string.
+ * @param {Record<string, unknown>} obj - The object to be converted into a query string.
  * @returns {string} The query string representation of the object.
  *
  * @example
  * const obj = { name: 'John', age: 30, city: 'New York' };
- * console.log(objectToQueryString(obj));  // Output: "name=John&age=30&city=New%20York"
+ * console.log(objectToQueryString(obj));  // Output: "name=John&age=30&city=New%20York"
  */
-export function objectToQueryString(obj: Record<string, string>): string {
+export function objectToQueryString(obj: Record<string, unknown>): string {
   const queryString = Object.entries(obj)
+    .filter(([_key, value]) => value !== undefined && value !== null) // Ignore undefined or null values
     .map(
       ([key, value]) =>
-        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-    )
+        `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`
+    ) // Ensure everything is a string
     .join("&");
   return queryString;
 }
